70fcb40095304ec2ec584bb63ed7a62e77b04f82
Angelo Rossi <yrc_legna@hotmail.com>
2025-07-06 22:01:18 -0300

    teste mediana

--- teste.py
+++ teste.py
@@ -3,9 +3,12 @@ import pandas as pd
 
 path = kagglehub.dataset_download("mathchi/diabetes-data-set")
 
-print("Path to dataset files:", path)
+#print("Path to dataset files:", path)
 
 df = pd.read_csv(path + "/diabetes.csv")
 
 print(df.shape) #(768, 9)
 
+median = df["Insulin"].median()
+
+print(median)
